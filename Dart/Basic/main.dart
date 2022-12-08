ff5fe9d4a63fa35fb240f02ad6ba65c646ab9083
Klaus M. Rodewig <kmr@appnoe.com>
2022-12-08 09:36:19 +0100

    type inference with ‚dynamic‘
    Signed-off-by: Klaus M. Rodewig <kmr@appnoe.com>

--- Dart/Basic/main.dart
+++ Dart/Basic/main.dart
@@ -1,4 +1,10 @@
 void main(List<String> args) {
-  var typeInference = 'World';
-  print('Hello $typeInference');
+  dynamic typeInference = 'World';
+  // Type is inferred by compiler
+  // value can be changed to value of ANY type
+  // dangerous / bad practice
+  typeInference = 23; // type changed to int
+  print('Hello ' + (typeInference * 2).toString());
+  typeInference = 'wat?'; // type changed to String
+  print('Hello ' + (typeInference * 2).toString()); // result is nonsense
 }
