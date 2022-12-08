e4b5da2937017bacbabaac925d4d089607ce573b
Klaus M. Rodewig <kmr@appnoe.com>
2022-12-08 09:38:24 +0100

    functions
    Signed-off-by: Klaus M. Rodewig <kmr@appnoe.com>

--- Dart/Basic/main.dart
+++ Dart/Basic/main.dart
@@ -1,10 +1,42 @@
 void main(List<String> args) {
-  dynamic typeInference = 'World';
-  // Type is inferred by compiler
-  // value can be changed to value of ANY type
-  // dangerous / bad practice
-  typeInference = 23; // type changed to int
-  print('Hello ' + (typeInference * 2).toString());
-  typeInference = 'wat?'; // type changed to String
-  print('Hello ' + (typeInference * 2).toString()); // result is nonsense
+  // function with parameters
+  print(userInfo("Franz", 42));
+
+  // function with optional parameters
+  print(userInfoWithState("Joe", 23, true));
+
+  // function with optional parameters
+  print(userInfoWithState("Jane", 24));
+
+  // function with parentheses
+  print(yesOrNo(1));
+
+  // function with with arrow syntax
+  print(yesOrNoArrow(1));
 }
+
+// function with parameters
+String userInfo(String name, int age) {
+  return "$name is $age years old";
+}
+
+// function with optional parameters
+String userInfoWithState(String name, int age, [bool? isOnline]) {
+  if (isOnline != null) {
+    return "$name is $age years old (🟢)";
+  } else {
+    return "$name is $age years old (🔴)";
+  }
+}
+
+// function with parentheses
+bool yesOrNo(int a) {
+  if (a == 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function with arrow syntax
+bool yesOrNoArrow(int a) => (a == 1) ? true : false;
